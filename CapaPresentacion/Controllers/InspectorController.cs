8ba1157be4567927c42e9b7138e9849fb9bace16
brandon.ramirez <brandonjrl11@gmail.com>
2025-05-06 11:51:00 -0500

    add Vistas (En blanco) Inspector

---
+++ CapaPresentacion/Controllers/InspectorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapaPresentacion.Controllers
+{
+    public class InspectorController : Controller
+    {
+        // GET: Inspector
+        public ActionResult RevisarDocumentacion()
+        {
+            return View();
+        }
+
+        public ActionResult Inspeccion()
+        {
+            return View();
+        }
+    }
+}
